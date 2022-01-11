b529bd3f0ac81dc554c79daf6032cd1726c17d15
Olivia <olivia.cowan@hotmail.com>
2022-01-11 01:57:21 +0000

    adding the head

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  let first = array[0]
+    if (array.length === 0) {
+      return (undefined);
+    } else if (array.length === 1) {
+     return (array[0]);
+    } else {
+      return first;
+  }
+}
+
+
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
