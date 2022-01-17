e8da979dbbceee09dfb548442d47af29d8f07d95
Olivia <olivia.cowan@hotmail.com>
2022-01-17 23:39:31 +0000

    object containing all lotide functions

---
+++ index.js
@@ -0,0 +1,31 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectEqual');
+const eqObjects = require('./eqObjects');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  map: map,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  countOnly: countOnly,
+  countLetters: countLetters
+};
