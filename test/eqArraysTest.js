12aa3b8bad6663adb7b1460e12b36f6896c18f03
Olivia <olivia.cowan@hotmail.com>
2022-01-17 18:15:03 +0000

    test refactored to use mocha chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,15 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, '2', 3]), false); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, '2', 3]), false); // => should PASS
+
+describe("#eqArrays", () => {
+  it("returns true for the inputs [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for the inputs [1, 2, 3] and [1, '2', 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, '2', 3]), false);
+  });
+});
