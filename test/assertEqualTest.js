29b4604dcfb3f66c06ec0561f4c63fc94c29c7cb
Olivia <olivia.cowan@hotmail.com>
2022-01-17 22:43:27 +0000

    implemented mocha chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,14 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
 
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+// assertEqual("Bootcamp", "Bootcamp");
+// assertEqual(1, 2);
+describe("#assertEqual", () => {
+  it('returns bootcamp for bootcamp', () => {
+    assert.strictEqual(assertEqual('bootcamp', 'bootcamp'), true);
+  });
+  it('should return false with the input (1, 2)', () => {
+    assert.strictEqual(assertEqual(1,2), false);
+  })
+});
