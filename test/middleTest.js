2eb7992f708a56c0c6dd8d1dae06eef34d0a5c64
Olivia <olivia.cowan@hotmail.com>
2022-01-17 22:44:12 +0000

    implemented mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 
-(assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4])); // => [3, 4]
-(assertArraysEqual((middle([1, 2, 3])), [2])); // => [2]
-(assertArraysEqual((middle([1, 2, 3, 4, 5])), [3])); // => [3]
-(assertArraysEqual((middle([1, 2, 3, 4, 5, 6, 7])), [4]))
+// (assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4])); // => [3, 4]
+// (assertArraysEqual((middle([1, 2, 3])), [2])); // => [2]
+// (assertArraysEqual((middle([1, 2, 3, 4, 5])), [3])); // => [3]
+// (assertArraysEqual((middle([1, 2, 3, 4, 5, 6, 7])), [4]))
+describe('#middle', () => {
+  it('returns [3, 4] when given the string [1, 2, 3, 4, 5, 6]', () => {
+    const actual = [1, 2, 3, 4, 5, 6];
+    const expected = [3, 4];
+    assert.deepEqual(middle(actual), expected);
+  });
+});
