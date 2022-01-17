a0ba02fc18570d9075b87e4993c043f75ee61c29
Olivia <olivia.cowan@hotmail.com>
2022-01-17 17:23:26 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i] || typeof arr1[i] !== typeof arr2[i]) {
-        return false;
-      } 
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
@@ -18,12 +9,5 @@ const assertArraysEqual = function(array1, array2) {
     return false;
   }
 };
-console.log(assertArraysEqual(['hey', 'ho'], ['hey', 'ho']));
+module.exports = assertArraysEqual;
 
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
-}; */
