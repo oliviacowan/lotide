120fa9d0822fc85dbc5476c1ed970cb1c43fe2eb
Olivia <olivia.cowan@hotmail.com>
2022-01-17 22:40:34 +0000

    final push

--- tail.js
+++ tail.js
@@ -1,4 +1,12 @@
-const assertEqual = require('./assertEqual')
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
 
 const tail = function(array) {
   if (array.length === 1) {
@@ -8,5 +16,5 @@ const tail = function(array) {
   }
   return array.slice(1);
 };
-module.exports = tail;
 
+module.exports = tail;
