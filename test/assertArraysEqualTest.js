4019fa17badb1b445d75f7713d75d035e123458c
Olivia <olivia.cowan@hotmail.com>
2022-01-17 17:22:52 +0000

    assertArraysEqual test

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual(['hey', 'ho'], ['hey', 'ho']);
+assertArraysEqual(['hey', 'hello'], ['hey', 'ho']);
+
+
