9e41248c7c47f066e5c1d9c8513c9d73fe7d41fb
Olivia <olivia.cowan@hotmail.com>
2022-01-17 22:42:44 +0000

    implemented mocha chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,14 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(['hey', 'ho'], ['hey', 'ho']);
-assertArraysEqual(['hey', 'hello'], ['hey', 'ho']);
+// assertArraysEqual(['hey', 'ho'], ['hey', 'ho']);
+// assertArraysEqual(['hey', 'hello'], ['hey', 'ho']);
 
+describe('#assertArraysEqual', () => {
+  it("returns ['hey', 'ho'] when given ['hey, 'ho']", () => {
+    
+    assert.strictEqual(assertArraysEqual(['hey', 'ho'], ['hey', 'ho']), true);
+  });
+});
 
